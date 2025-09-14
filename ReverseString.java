13daa217705d5cc6c344e31bf69500727e50b0d5
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-14 06:58:18 +0000

    container config done

--- ReverseString.java
+++ ReverseString.java
@@ -1,1 +1,19 @@
 //------------Reverse a string without using built-in functions.-----------
+public class ReverseString{
+    public static void main (String[] args){
+        String s = "hello world";
+        char[] arr = s.toCharArray();
+        int left = 0, right = arr.length - 1;
+        
+        while(left < right){
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        String reversed = new String(arr);
+        System.out.println( s);
+        System.out.println( reversed);
+    }
+}
