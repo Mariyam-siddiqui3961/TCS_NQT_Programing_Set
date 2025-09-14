a73c753ca3d3da131b2bb9208a98546e3bd4c58c
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-14 12:04:29 +0000

    container config done

--- PalindromeString.java
+++ PalindromeString.java
@@ -1,1 +1,25 @@
 //------------Check if a string is a palindrome------------------
+public class PalindromeCheck{
+    public static void main (String[] args){
+        String s = "madam";
+        
+        if (isPalindrome(s)){
+            System.out.println(s + " is palindrome");
+        }else{
+            System.out.println(s + " is not palindrome");
+        }
+    }
+    public static boolean isPalindrome(String s){
+        int left = 0,  right = s.length()-1;
+        
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+        
+    }
+}
