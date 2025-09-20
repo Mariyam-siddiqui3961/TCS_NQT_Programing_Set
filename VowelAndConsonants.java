99acadfd357f7cf252df234d93359bc731bf7304
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-20 17:22:24 +0000

    container config done

--- VowelAndConsonants.java
+++ VowelAndConsonants.java
@@ -1,1 +1,25 @@
 //-------------Count the number of vowels and consonants in a string.-------
+public class vowelConsonants{
+    public static void main(String[] args){
+        String str = "Hello World";
+        str = str.toLowerCase();
+        
+        int vowels = 0, consonants = 0;
+        
+        for (int i = 0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            
+            if (ch >='a' && ch<='z'){
+                if(ch == 'a' || ch == 'e' || ch == 'i' || ch =='o' || ch == 'u'){
+                    vowels++;
+                }else{
+                    consonants++;
+                }
+            }
+        }
+        System.out.println(vowels);
+        System.out.println(consonants);
+        
+        
+    }
+}
