9598edd055feddfa6d0422de74dff6cc5fe34a2e
Mariyam Siddiqui <siddiquimariyam9044@gmail.com>
2025-09-23 18:15:49 +0000

    container config done

--- sqrtx.java
+++ sqrtx.java
@@ -1,1 +1,24 @@
 //-------------------sqrtx----------------------
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0 || x == 1) {
+            return x;
+        }
+        
+        int left = 1;
+        int right = x;
+        int result = 0;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+             if (mid * mid <= x ) {
+                left = mid + 1;
+                result = mid; 
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return result;
+    }
+}
